10a45f4083ba7a72df8046d7ff9a1a14e9179f65
Andrew Struk <andrewstruk@yahoo.co.uk>
2024-06-23 11:58:29 +0000

    learned how change classes with hooks

--- my-app/src/components/ContolledFormHooks.js
+++ my-app/src/components/ContolledFormHooks.js
@@ -1,8 +1,53 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function ContolledFormHooks() {
+  const [name, setName] = useState('');
+  const [category, setCategory] = useState('website');
+  const [comments, setComments] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, category, comments)
+  }
   return (
-    <div>It works!!  </div>
+    <div>
+    <h2>Please fill out the form below:</h2>
+    <form onSubmit={handleSubmit}>
+        <div>
+            <label htmlFor="id-name">Your Name:</label>
+            <input
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                id="id-name"
+                name="name"
+                type="text"
+            />
+        </div>
+        <div>
+            <label htmlFor="id-category">Query category:</label>
+            <select
+                id="id-category"
+                name="category"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+            >
+                <option value="website">Website issue</option>
+                <option value="order">Order issue</option>
+                <option value="general">General inquiry</option>
+
+            </select>
+        </div>
+        <div>
+            <label htmlFor="id-comments">Comments:</label>
+            <textarea
+                id="id-comments"
+                name="comments"
+                value={comments}
+                onChange={(e) => setComments(e.target.value)}
+            />
+        </div>
+        <input type="submit" value="Submit" />
+    </form>
+</div>
   )
 }
 
