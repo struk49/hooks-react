9a8ea5e6cebe2aababffb551f93517449435ebed
Andrew Struk <andrewstruk@yahoo.co.uk>
2024-06-23 10:56:43 +0000

    initail commit

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import ControlledForm from './components/ControlledForm';
+import ContolledFormHooks from './components/ContolledFormHooks';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <ControlledForm />
+      <ContolledFormHooks />
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/ContolledFormHooks.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ContolledFormHooks() {
+  return (
+    <div>It works!!  </div>
+  )
+}
+
+export default ContolledFormHooks
